faa65d8cd4b79bc20da3d30812d764d4f8826007
Hans de Rooij <HansDeRooij@Hotmail.com>
2022-12-03 20:16:47 +0100

    Initial structure created

---
+++ index.js
@@ -0,0 +1,26 @@
+// *********************************************************************
+//
+// D&B Direct+ utilities (v3) main file as defined in package.json
+// JavaScript code file: index.js
+//
+// Copyright 2022 Hans de Rooij
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the
+// License.
+//
+// *********************************************************************
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.ENV_VAR);
